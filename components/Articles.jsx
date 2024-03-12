bf51cd7f9dca8e10639827ee1a21198841f37562
Drodinger <ri3yne@hotmail.com>
2024-03-12 12:33:12 +0000

    WIP list-all-articles 2

--- components/Articles.jsx
+++ components/Articles.jsx
@@ -1,23 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import axios from 'axios';
 import ArticlesList from './ArticlesList.jsx';
 
 
 export default function Articles() {
-    const [articles, setArticles] = useState([]);
-    const [topic, setTopic] = useState(null);
     const [searchParams, setSearchParams] = useSearchParams();
 
-    useEffect(() => {
-        const topic = searchParams.get("topic");
-        axios.get(topic ? `https://channel-5-news.onrender.com/api/articles?topic=${topic}` : "https://channel-5-news.onrender.com/api/articles")
-            .then((res) => {
-                setArticles(res.data.articles);
-            })
-    }, [])
-
     return (
-        <ArticlesList />
+        <ArticlesList topic={searchParams.get("topic")}/>
     )
 }
--- components/ArticlesList.jsx
+++ components/ArticlesList.jsx
@@ -1,30 +1,41 @@
-import { useState } from 'react';
-import articles from './Articles.jsx';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
-export default function ArticlesList(articles) {
+export default function ArticlesList(topic) {
+    const [articles, setArticles] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(topic.topic ? `https://channel-5-news.onrender.com/api/articles?topic=${topic.topic}` : "https://channel-5-news.onrender.com/api/articles")
+            .then((res) => {
+                setArticles(res.data.articles);
+                setIsLoading(false);
+            })
+    }, [])
+
     if (isLoading) {
         return (
             <p>Loading...</p>
         )
-    } else {
+    } 
         return (
             <>
-                {
-                    articles.map((article) => {
-                            <li className="articles-list-item">
-                            <h2>article.title</h2>
-                            <div className="articles-list-item .details">
-                            <p>{article.created_at}</p>
-                            <p>Author: {article.author}</p>
-                            <p>{article.votes} votes</p>
-                            <p>{article.comment_count} comments</p>
-                            </div>
-                            </li>
-                    })
-                }
+                <ul>
+                    {
+                        articles.map((article) => {
+                                <li className="articles-list-item">
+                                <h2>article.title</h2>
+                                <div className="articles-list-item-details">
+                                    <p>{article.created_at}</p>
+                                    <p>Author: {article.author}</p>
+                                    <p>{article.votes} votes</p>
+                                    <p>{article.comment_count} comments</p>
+                                </div>
+                                </li>
+                        })
+                    }
+                </ul>
             </>
         )
-    }
 }
